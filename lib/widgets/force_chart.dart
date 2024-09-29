ede84a41030c307293c539550166d69ab31ab40e
edward <edward5419@naver.com>
2024-09-29 14:19:37 +0800

    On branch main Changes to be committed: 	modified:   lib/widgets/force_chart.dart

--- lib/widgets/force_chart.dart
+++ lib/widgets/force_chart.dart
@@ -1,6 +1,5 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'dart:math' show max;
 
 class ForceChart extends StatelessWidget {
   final List<double> selectedDataStream1;
@@ -14,26 +13,18 @@ class ForceChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // 최대 표시할 데이터 포인트 수
-    final int maxDisplayPoints = 500;
-
-    // 두 데이터 스트림 중 짧은 길이를 기준으로 설정
+    // 데이터 포인트 생성
+    final int maxDisplayPoints = 100; // 최대 표시할 데이터 포인트 수
     final int len1 = selectedDataStream1.length;
     final int len2 = selectedDataStream2.length;
     final int len = len1 < len2 ? len1 : len2;
     final int displayLength = len < maxDisplayPoints ? len : maxDisplayPoints;
 
-    // 데이터가 없는 경우 표시할 위젯
-    if (len < 1) {
-      return Center(child: Text('No data to display'));
-    }
-
-    // 두 데이터 스트림에서 표시할 데이터 포인트 생성
     final List<FlSpot> spots1 = List.generate(
       displayLength,
       (index) => FlSpot(
         index.toDouble(),
-        selectedDataStream1[index],
+        selectedDataStream1[len1 - displayLength + index],
       ),
     );
 
@@ -41,11 +32,11 @@ class ForceChart extends StatelessWidget {
       displayLength,
       (index) => FlSpot(
         index.toDouble(),
-        selectedDataStream2[index],
+        selectedDataStream2[len2 - displayLength + index],
       ),
     );
 
-    // Y축의 최소 및 최대값 계산
+    // 최대 최소 Y값 계산
     double minY = 0;
     double maxY = 0;
     if (spots1.isNotEmpty && spots2.isNotEmpty) {
@@ -60,31 +51,16 @@ class ForceChart extends StatelessWidget {
       ].reduce((a, b) => a > b ? a : b);
     }
 
-    // Y축 범위 조정하여 간격이 0이 되지 않도록 함
-    if (minY == maxY) {
-      // 모든 Y값이 동일한 경우 기본 범위 설정
-      minY = minY - 1;
-      maxY = maxY + 1;
-    } else {
-      // Y축 범위에 여유 공간 추가
-      minY = minY * 0.8;
-      maxY = maxY * 1.2;
-    }
-
-    // Y축 간격 계산
-    double horizontalInterval = (maxY - minY) / 5;
-
-    // 간격이 0이 되지 않도록 기본값 설정
-    if (horizontalInterval == 0) {
-      horizontalInterval = 1.0;
-    }
+    // 여유 공간 추가
+    minY = minY * 0.8;
+    maxY = maxY * 1.2;
 
     return LineChart(
       LineChartData(
         gridData: FlGridData(
           show: true,
-          drawVerticalLine: false,
-          horizontalInterval: horizontalInterval,
+          drawVerticalLine: true,
+          horizontalInterval: (maxY - minY) / 5,
           verticalInterval: displayLength / 5,
           getDrawingHorizontalLine: (value) {
             return FlLine(
@@ -112,7 +88,7 @@ class ForceChart extends StatelessWidget {
               showTitles: true,
               reservedSize: 22,
               getTitlesWidget: bottomTitleWidgets,
-              interval: max(1, (displayLength / 5).floorToDouble()),
+              interval: displayLength / 5,
             ),
           ),
           leftTitles: AxisTitles(
@@ -120,7 +96,7 @@ class ForceChart extends StatelessWidget {
               showTitles: true,
               getTitlesWidget: leftTitleWidgets,
               reservedSize: 40,
-              interval: horizontalInterval,
+              interval: (maxY - minY) / 5,
             ),
           ),
         ),
@@ -131,56 +107,44 @@ class ForceChart extends StatelessWidget {
           ),
         ),
         minX: 0,
-        maxX: 500,
+        maxX: displayLength.toDouble(),
         minY: minY,
         maxY: maxY,
         lineBarsData: [
           LineChartBarData(
             spots: spots1,
-            isCurved: false,
+            isCurved: true,
             gradient: const LinearGradient(
               colors: [
                 Color(0xff23b6e6),
                 Color(0xff02d39a),
               ],
             ),
-            barWidth: 3,
+            barWidth: 2,
             isStrokeCapRound: true,
-            dotData: FlDotData(
+            dotData: const FlDotData(
               show: false,
             ),
             belowBarData: BarAreaData(
-              show: true,
-              gradient: LinearGradient(
-                colors: [
-                  Color(0xff23b6e6).withOpacity(0.3),
-                  Color(0xff02d39a).withOpacity(0.3),
-                ],
-              ),
+              show: false,
             ),
           ),
           LineChartBarData(
             spots: spots2,
-            isCurved: false,
+            isCurved: true,
             gradient: const LinearGradient(
               colors: [
                 Color(0xfffa0000),
                 Color(0xffffdd00),
               ],
             ),
-            barWidth: 3,
+            barWidth: 2,
             isStrokeCapRound: true,
-            dotData: FlDotData(
+            dotData: const FlDotData(
               show: false,
             ),
             belowBarData: BarAreaData(
-              show: true,
-              gradient: LinearGradient(
-                colors: [
-                  Color(0xfffa0000).withOpacity(0.3),
-                  Color(0xffffdd00).withOpacity(0.3),
-                ],
-              ),
+              show: false,
             ),
           ),
         ],
@@ -195,36 +159,11 @@ class ForceChart extends StatelessWidget {
       fontWeight: FontWeight.bold,
       fontSize: 12,
     );
-    String text;
-
-    // 원하는 X축 라벨을 설정 (예시: 특정 값에 이름 부여)
-    switch (value.toInt()) {
-      case 0:
-        text = 'JAN';
-        break;
-      case 2:
-        text = 'MAR';
-        break;
-      case 4:
-        text = 'MAY';
-        break;
-      case 6:
-        text = 'JUL';
-        break;
-      case 8:
-        text = 'SEP';
-        break;
-      case 10:
-        text = 'NOV';
-        break;
-      default:
-        text = '';
-        break;
-    }
+    Widget text = Text(value.toInt().toString(), style: style);
 
     return SideTitleWidget(
       axisSide: meta.axisSide,
-      child: Text(text, style: style),
+      child: text,
     );
   }
 
@@ -236,7 +175,6 @@ class ForceChart extends StatelessWidget {
       fontSize: 12,
     );
     String label = value.toStringAsFixed(1);
-
     return Text(label, style: style, textAlign: TextAlign.right);
   }
 }
